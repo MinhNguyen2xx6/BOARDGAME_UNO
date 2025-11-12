5e387152f8b1dd6aa5ab7f89e23e723dc53cc88d
Phukuit <vothanhphuc672006@gmail.com>
2025-11-12 18:50:17 +0700

    Clean project structure

---
+++ UNO_Client/Giaodienchinh.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace UNO_Client
+{
+    partial class Giaodienchinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            btn_Play = new Button();
+            btn_login = new Button();
+            label_tennguoidung = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.d841d3fa_a631_4ecb_af3c_5c6cdbea61a4;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1319, 961);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btn_Play
+            // 
+            btn_Play.Location = new Point(567, 575);
+            btn_Play.Name = "btn_Play";
+            btn_Play.Size = new Size(167, 91);
+            btn_Play.TabIndex = 1;
+            btn_Play.Text = "Play";
+            btn_Play.UseVisualStyleBackColor = true;
+            btn_Play.Click += btn_Play_Click;
+            // 
+            // btn_login
+            // 
+            btn_login.Location = new Point(12, 12);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(186, 50);
+            btn_login.TabIndex = 2;
+            btn_login.Text = "Đăng nhập";
+            btn_login.UseVisualStyleBackColor = true;
+            btn_login.Click += btn_login_Click;
+            // 
+            // label_tennguoidung
+            // 
+            label_tennguoidung.AutoSize = true;
+            label_tennguoidung.Location = new Point(1166, 42);
+            label_tennguoidung.Name = "label_tennguoidung";
+            label_tennguoidung.Size = new Size(50, 20);
+            label_tennguoidung.TabIndex = 3;
+            label_tennguoidung.Text = "label1";
+            // 
+            // Giaodienchinh
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1281, 966);
+            Controls.Add(label_tennguoidung);
+            Controls.Add(btn_login);
+            Controls.Add(btn_Play);
+            Controls.Add(pictureBox1);
+            Name = "Giaodienchinh";
+            Text = "Giaodienchinh";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button btn_Play;
+        private Button btn_login;
+        private Label label_tennguoidung;
+    }
+}
---
+++ UNO_Client/Lobby.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UNO_Client
+{
+    public partial class Lobby : Form
+    {
+        public Lobby()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_dangxuat_Click(object sender, EventArgs e)
+        {
+            Session.IdToken = null;
+            Session.RefreshToken = null;
+            Session.UserEmail = null;
+            Form1 loginForm = new Form1();
+            loginForm.Show();
+            this.Close();
+        }
+    }
+}
