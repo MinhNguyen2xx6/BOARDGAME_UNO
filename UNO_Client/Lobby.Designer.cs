a4d328c1c05a90960c32ed509e819df975902a30
MinhNguyen2xx6 <quangminhanbu@ms.uit.edu.vn>
2025-11-17 01:39:00 +0700

    Chỉnh sửa sau khi pull từ nhánh khác

--- UNO_Client/Lobby.Designer.cs
+++ UNO_Client/Lobby.Designer.cs
@@ -35,14 +35,17 @@
             label2 = new Label();
             tbRoomName = new TextBox();
             btn_back = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             SuspendLayout();
             // 
             // btnCreateRoom
             // 
-            btnCreateRoom.Location = new Point(107, 115);
+            btnCreateRoom.Location = new Point(84, 91);
             btnCreateRoom.Margin = new Padding(2);
             btnCreateRoom.Name = "btnCreateRoom";
-            btnCreateRoom.Size = new Size(94, 34);
+            btnCreateRoom.Size = new Size(82, 26);
             btnCreateRoom.TabIndex = 1;
             btnCreateRoom.Text = "Tạo Phòng";
             btnCreateRoom.UseVisualStyleBackColor = true;
@@ -50,10 +53,10 @@
             // 
             // btnReset
             // 
-            btnReset.Location = new Point(392, 65);
+            btnReset.Location = new Point(343, 49);
             btnReset.Margin = new Padding(2);
             btnReset.Name = "btnReset";
-            btnReset.Size = new Size(94, 34);
+            btnReset.Size = new Size(82, 26);
             btnReset.TabIndex = 2;
             btnReset.Text = "Reset";
             btnReset.UseVisualStyleBackColor = true;
@@ -62,54 +65,90 @@
             // lstRooms
             // 
             lstRooms.FormattingEnabled = true;
-            lstRooms.Location = new Point(370, 140);
+            lstRooms.ItemHeight = 15;
+            lstRooms.Location = new Point(324, 90);
             lstRooms.Margin = new Padding(2);
             lstRooms.Name = "lstRooms";
-            lstRooms.Size = new Size(145, 104);
+            lstRooms.Size = new Size(329, 259);
             lstRooms.TabIndex = 3;
             // 
             // lblRoomName
             // 
             lblRoomName.AutoSize = true;
-            lblRoomName.Location = new Point(38, 81);
+            lblRoomName.Location = new Point(33, 61);
             lblRoomName.Margin = new Padding(2, 0, 2, 0);
             lblRoomName.Name = "lblRoomName";
-            lblRoomName.Size = new Size(118, 20);
+            lblRoomName.Size = new Size(96, 15);
             lblRoomName.TabIndex = 4;
             lblRoomName.Text = "Nhập Tên Phòng";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(38, 129);
+            label2.Location = new Point(33, 97);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(0, 20);
+            label2.Size = new Size(0, 15);
             label2.TabIndex = 5;
             // 
             // tbRoomName
             // 
-            tbRoomName.Location = new Point(158, 78);
+            tbRoomName.Location = new Point(138, 58);
             tbRoomName.Margin = new Padding(2);
             tbRoomName.Name = "tbRoomName";
-            tbRoomName.Size = new Size(121, 27);
+            tbRoomName.Size = new Size(106, 23);
             tbRoomName.TabIndex = 6;
             // 
             // btn_back
             // 
-            btn_back.Location = new Point(38, 432);
+            btn_back.Location = new Point(33, 324);
+            btn_back.Margin = new Padding(3, 2, 3, 2);
             btn_back.Name = "btn_back";
-            btn_back.Size = new Size(94, 29);
+            btn_back.Size = new Size(82, 22);
             btn_back.TabIndex = 7;
             btn_back.Text = "Quay Về";
             btn_back.UseVisualStyleBackColor = true;
             btn_back.Click += btn_back_Click;
             // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(468, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 36);
+            button1.TabIndex = 8;
+            button1.Text = "JOIN";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(197, 91);
+            button2.Name = "button2";
+            button2.Size = new Size(84, 26);
+            button2.TabIndex = 9;
+            button2.Text = "Thoát phòng";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(197, 144);
+            button3.Name = "button3";
+            button3.Size = new Size(84, 29);
+            button3.TabIndex = 10;
+            button3.Text = "Xóa phòng";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
             // Lobby
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1080, 576);
+            ClientSize = new Size(945, 432);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(btn_back);
             Controls.Add(tbRoomName);
             Controls.Add(label2);
@@ -117,6 +156,7 @@
             Controls.Add(lstRooms);
             Controls.Add(btnReset);
             Controls.Add(btnCreateRoom);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Lobby";
             Text = "Lobby";
             Load += Lobby_Load;
@@ -132,5 +172,8 @@
         private Label label2;
         private TextBox tbRoomName;
         private Button btn_back;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- UNO_Client/obj/Debug/net8.0-windows/UNO_Client.AssemblyInfo.cs
+++ UNO_Client/obj/Debug/net8.0-windows/UNO_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UNO_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90371e4982732033c428ee5d7111d0c8996b693b")]
 [assembly: System.Reflection.AssemblyProductAttribute("UNO_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UNO_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
